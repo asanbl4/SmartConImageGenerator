0b1eeef2e11085e533483a752bd50e517c1b18ba
assanalibatyrkhan <asankek12@gmail.com>
2024-10-30 05:51:26 +0800

    img fix

--- file_converter.py
+++ file_converter.py
@@ -1,5 +1,4 @@
 from pdf2image import convert_from_path
-from PIL import Image
 
 
 def convert_pdf_to_jpg(pdf_path, output_path):
@@ -8,9 +7,6 @@ def convert_pdf_to_jpg(pdf_path, output_path):
 
     for i, image in enumerate(images):
         cropped_image = image.crop((0, 0, 1600, 768))
-        resized_image = cropped_image.resize((900, 384), Image.Resampling.LANCZOS)
-        resized_image.save(output_path, 'PNG')
+        cropped_image.save(output_path, 'PNG')
         image_paths.append(output_path)
         print(f"Saved page {i + 1} as PNG at {output_path}")
-
-    return image_paths
