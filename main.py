5fea75c3c4236086c7773ff1248e7a706e0d776b
assanalibatyrkhan <asankek12@gmail.com>
2024-10-30 22:57:32 +0800

    headers fix

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from file_converter import convert_pdf_to_jpg
 from downloader import download_doc_as_pdf
 from spaceandtime import send_sql
 from poe import send_prompt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 async def fetch_sql_data(query):
@@ -18,12 +22,12 @@ async def main(query, example):
 
 if __name__ == '__main__':
     for i in range(1, 11):
-        if_dark = True
-        if not i % 2:
-            if_dark = False
-        header_text = ""
-        body_text = ""
-        doc_id = update_document_content(i, if_dark)
-        download_doc_as_pdf(doc_id=doc_id, output_path=f"IMG{i}.pdf")
+        # if_dark = True
+        # if not i % 2:
+        #     if_dark = False
+        # header_text = ""
+        # body_text = ""
+        # doc_id = update_document_content(i, if_dark)
+        download_doc_as_pdf(doc_id=os.getenv(f"IMG{i}_ID"), output_path=f"IMG{i}.pdf")
         convert_pdf_to_jpg(f"IMG{i}.pdf", f"IMG{i}.png")
         firebase_url = upload_image_to_firebase(f"IMG{i}.png")
